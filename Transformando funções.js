9956417252afedf66255b1b3ff95baa2c49ed799
Jorge Silva <89113954+jorgehpsilva@users.noreply.github.com>
2021-09-17 23:48:41 -0300

    Add files via upload

---
+++ Transformando funções.js
@@ -0,0 +1,16 @@
+// function print(mensagem){
+//     console.log(mensagem)
+//   }   
+//   print("Olá, bom dia")
+// function soma(n1, n2){
+//     return n1 + n2
+//   }
+//   console.log(soma(10, 10));
+console.log ("Funções transformadas com sucesso");  
+
+  let mensagem = () => "Olá, bom dia!"; 
+  let soma = (n1, n2) => n1 + n2;
+  
+  console.log(mensagem());
+  console.log(soma(10, 10));
+  
